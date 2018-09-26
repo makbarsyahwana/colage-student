2e1e81fb26d861add2cc5a45b92acd182706214e
makbarsyahwana <makbarsyahwana@gmail.com>
2018-09-26 14:56:59 +0700

    change port

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ app.use(parser.urlencoded({ extended: false }))
 app.use(cors())
 app.get("/", (req, res) => res.send("Hello World!"))
 
-app.set("port", process.env.PORT || 3000)
-
-app.listen(app.get("port"), () => {
+app.listen(3000, () => {
   console.log("Hello world!")
 })
